bae6429138333cf51c25caa6c8ed64e77525b4d1
cch5ng <cch5ng@gmail.com>
2015-08-13 17:58:50 -0700

    added displayStories() function. only tested for one storyObj so far but it is displaying the content expected. need to test for the entire array of objs. then also reformat it for display

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@
 // result.upvotes: bonus 
 
 //data model
-
+var stories = [];
+var requestUrl = 'http://www.freecodecamp.com/stories/hotStories';
 
 //helper function
 //given a story's native storyLink, parses the string to the format necessary to create a FreeCodeCamp discussion page link
@@ -19,8 +20,37 @@ function parseDiscussLink(str) {
   return strAr.join('-');  
 }
 
-var stories = [];
-var requestUrl = 'http://www.freecodecamp.com/stories/hotStories';
+function displayStories(storyObj) {
+
+  var main = document.querySelector('main');
+  var article = document.createElement('article');
+
+  var a = document.createElement('a');
+  a.setAttribute('href', storyObj.link);
+  a.setAttribute('target', '_blank');
+  a.innerText = storyObj.headline;
+
+  var h2 = document.createElement('h2');
+  h2.appendChild(a);
+
+  var p = document.createElement('p');
+  var a2 = document.createElement('a');
+  a2.setAttribute('href',  storyObj.discussLink);
+  a2.setAttribute('target', '_blank');
+  a2.innerText = 'Discuss';
+  p.appendChild(a2);
+  console.log(p);
+
+  var p2 = document.createElement('p');
+  p2.innerText = storyObj.upvotes + ' upvotes';
+
+  article.appendChild(h2);
+  article.appendChild(p);
+  article.appendChild(p2);
+  main.appendChild(article);
+
+}
+
 
 var jqxhr = $.ajax({
   url: requestUrl,
@@ -47,6 +77,7 @@ error(function(error) {
 }).
 always(function() {
   console.log(stories);
+  displayStories(stories[0]);
   //console.log(stories[0]);
   //console.log(stories[49]);
 });
